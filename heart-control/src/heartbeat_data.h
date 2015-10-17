4e6ac7f813a16b3c07d5bda772b8a6304ca99f7d
Joseph Lunderville <jlunder@catfish.dhs.org>
2015-10-17 11:25:29 -0700

    Audio data

---
+++ heart-control/src/heartbeat_data.h
@@ -0,0 +1,13 @@
+/*
+ * heartbeat_data.h
+ *
+ *  Created on: Oct 16, 2015
+ *      Author: jlunder
+ */
+
+#ifndef HEARTBEAT_DATA_H_
+#define HEARTBEAT_DATA_H_
+
+uint8_t hd_heartbeat_data[24186];
+
+#endif /* HEARTBEAT_DATA_H_ */
